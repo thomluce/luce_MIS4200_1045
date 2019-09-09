1d364739f0f4c6870643cd08ec72ee9c14f61b3b
Luce, Thom <luce@ohio.edu>
2019-09-09 11:23:01 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(luce_MIS4200_1045.Startup))]
+namespace luce_MIS4200_1045
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
