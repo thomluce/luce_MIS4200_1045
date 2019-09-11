8c95255024876da50d85f10018722fac6ad1579f
Luce, Thom <luce@ohio.edu>
2019-09-11 11:39:41 -0400

    Initial model work

---
+++ Models/order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace luce_MIS4200_1045.Models
+{
+    public class order
+    {
+        public int orderID { get; set; }
+        public int customerID { get; set; }
+        public virtual customer customer { get; set; }
+        public DateTime orderDate { get; set; }
+    }
+}
